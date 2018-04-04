ccad43d94a97d94852f87ae0648b414244dfe7ce
Hyago Souza <hyagosouzza@hotmail.com>
2018-04-04 10:14:30 -0300

    040418

--- public/app.js
+++ public/app.js
@@ -11,25 +11,12 @@
     };
     firebase.initializeApp(config);
 
-    var app = angular.module("app", ["firebase"]);
+    var app = angular.module("app", ["firebase", "ngMask"]);
 
     app.controller("MyCtrl", ["$scope", "$firebaseArray",
         function ($scope, $firebaseArray) {
 
-            var db = firebase.firestore();
-            db.collection("Marcas")
-                .onSnapshot(function (querySnapshot) {
-                    $scope.mr = [];
-                    querySnapshot.forEach(function (doc) {
-                        const data = doc.data()
-                        data.id = doc.id;
-                        $scope.mr.push(data);
-                    });
-                });
-
-            var refMarca = firebase.database().ref();
-
-            var objMarca = $firebaseArray(refMarca);
+            $scope.maskPhone = "";
 
         }
     ]);
